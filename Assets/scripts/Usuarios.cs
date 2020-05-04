0ffb490b63dfcc789c92b8b3e2b29e23e171a2ea
jose <josemb125@gmail.com>
2020-05-04 11:03:14 -0500

    agregando clases iniciales

---
+++ Assets/scripts/Usuarios.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Usuarios : MonoBehaviour
+{
+    string nombre;
+    int nivel;
+    int experiencia;
+    string hermandad;
+    bool tutorialCompleto;
+    Usuarios[] amigos;
+    //Personajes[] personajes;
+    //Monedas[] monedas;
+    //Configuracion[] configuracion;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
